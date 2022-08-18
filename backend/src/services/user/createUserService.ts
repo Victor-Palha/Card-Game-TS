354395224b0ffcc2c228fcc3f15b74b3ca0c53e7
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-08-18 18:55:42 -0300

    Create users

--- backend/src/services/user/createUserService.ts
+++ backend/src/services/user/createUserService.ts
@@ -1,12 +1,53 @@
+//Import prismaClient
+import prismaClient from "../../prisma/"
+
 interface UserRequest{
-    name: String,
-    email: String,
-    password: String
+    name: string,
+    email: string,
+    password: string
 }
 
 class CreateUserService{
     async execute({name,email,password}:UserRequest){
-        return {name: name, email: email, password: password}
+        //Verifications
+            //Have email?
+        if(!email){
+            throw new Error("Email missing")
+        }
+            //Email already in database?
+        const emailAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+        if(emailAlreadyExists){
+            throw new Error("Email Already Exists!")
+        }
+            //UserName already exists?
+        const nameAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                username: name
+            }
+        })
+        if(nameAlreadyExists){
+            throw new Error("Username Already Exists!")
+        }
+        //Create new User
+        const user = await prismaClient.user.create({
+            data:{
+                username: name,
+                email: email,
+                password: password
+            },
+            select:{
+                username: true,
+                email: true,
+                id:true
+            }
+        })
+
+        //Return user
+        return {user}
     }
 }
 export {CreateUserService}

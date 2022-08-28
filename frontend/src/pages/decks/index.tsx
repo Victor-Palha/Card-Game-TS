1df884d6e9da5fbe94bc94feb204feac10c30fc4
João Victor Ferreira Palha <jv.palha@gmail.com>
2022-08-28 14:43:18 -0300

    deck page

--- frontend/src/pages/decks/index.tsx
+++ frontend/src/pages/decks/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Link from "next/link"
 import { toast } from "react-toastify"
+import { useState, useEffect } from "react"
 import styles from './style.module.scss'
 
 //Components
@@ -9,8 +10,25 @@ import { Header } from "../../components/Header"
 //Contexts
 import { canSSRAuth } from "../../utils/canSSRAuth"
 import { AuthContext } from '../../contexts/AuthContext'
+import { api } from "../../services/errors/apiClient"
 
 export default function Decks(){
+    const [decks, setDecks] = useState([])
+    const [loading, setLoading] = useState(true)
+    useEffect(()=>{
+        async function loadDecks(){
+            const response = await api.get('/decks')
+            setDecks(response.data)
+            setLoading(false)
+        }
+        loadDecks()
+    },[])
+    if(loading){
+        <div>
+            <h1>Carregando Decks...</h1>
+        </div>
+    }
+
     return(
         <>
         <Head>
@@ -19,7 +37,19 @@ export default function Decks(){
         <div>
             <Header/>
         </div>
-        
+        <section className={styles.decks}>
+        <div className='conteiner'>
+                <div className='lista-decks'>
+                    {decks.map((deck)=>{
+                        return(
+                            <article key={deck.id}>
+                                <h1><Link href={`/deck/${deck.id}`}>{deck.name}</Link></h1>
+                            </article>
+                        )
+                    })}
+                </div>
+            </div>
+        </section>
         </>
     )
 }
